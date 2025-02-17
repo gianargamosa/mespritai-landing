3965348e9cb8490e2287b7b236faec87d2ce3e27
Arman Gian Argamosa <argamosa.gian@gmail.com>
2025-02-17 09:29:25 +0800

    Add cta section

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,27 +1,22 @@
 import { infos } from "@/config/landing";
-import BentoGrid from "@/components/sections/bentogrid";
 import Faq from "@/components/sections/faq";
-import Features from "@/components/sections/features";
 import HeroLanding from "@/components/sections/hero-landing";
 import HowItWorks from "@/components/sections/how-it-works";
 import InfoLanding from "@/components/sections/info-landing";
-import Powered from "@/components/sections/powered";
 import PreviewLanding from "@/components/sections/preview-landing";
 import Testimonials from "@/components/sections/testimonials";
+import CtaSection from "@/components/shared/cta-section";
 
 export default function IndexPage() {
   return (
     <>
       <HeroLanding />
       <PreviewLanding />
-      {/* <Powered /> */}
       <HowItWorks />
-      {/* <BentoGrid /> */}
       <InfoLanding data={infos[0]} reverse={true} />
-      {/* <InfoLanding data={infos[1]} /> */}
-      {/* <Features /> */}
       <Testimonials />
       <Faq />
+      <CtaSection />
     </>
   );
 }
